dd14e1c9a75b202bc3804148b67437fa776a8c96
Oleksandr Hvozdyk <oleksandr.hvozdyk@gmail.com>
2019-03-03 21:15:28 +0200

    [Exception filters]
    - Create and tie to Cats Controller Http Exception Filter (https://docs.nestjs.com/exception-filters)

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { HttpExceptionFilter } from '../common/filters/http-exception.filter';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
   @Post()
